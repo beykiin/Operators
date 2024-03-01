f5585159756650dfe75128fd3b0062acdb45dcfc
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 15:09:19 +0300

    First Commit

---
+++ Operator.js
@@ -0,0 +1,18 @@
+// "==" The equality operator returns True.
+// "!=" The inequality operator (!=) compares two values and returns False if the values are equal, and True if they are not equal.
+// "+=" The augmented assignment operator (+=) is used to add a value to a variable. The operator takes the current value of the variable, adds it to the value on the right-hand side, and assigns the result to the variable.
+// "-=" The augmented assignment operator (-=) is used to subtract a value from a variable. The operator takes the current value of the variable, subtracts the value on the right-hand side, and assigns the result to the variable.
+// "%" The modulo operator (%) returns the remainder of the division of one number by another.
+// "%" The modulo operator (%) returns the remainder of the division of one number by another.
+// ">" Used in numerical comparisons
+// ">=" Used in numerical comparisons
+// "<" Used in numerical comparisons
+// "<=" Used in numerical comparisons
+// "*=" The multiplication and assignment operator (*=) multiplies the value of a variable by a number and assigns the result back to the variable.
+// "/=" The division and assignment operator (/=) divides the value of a variable by a number and assigns the result back to the variable.
+// "/" The division operator (/) divides two numbers.
+// "++" The increment and assignment operator (++) increments the value of a variable by 1 and assigns the result back to the variable.
+// "--" The decrement and assignment operator (--) decrements the value of a variable by 1 and assigns the result back to the variable.
+// "**" Used ​​For Exponential Number
+// "&&" and
+// "||" or
